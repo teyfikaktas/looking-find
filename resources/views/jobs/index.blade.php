124e6d5b1fff549636e51be87b17a5287cd72155
teyfikaktas <aktasteyf@gmail.com>
2025-01-29 01:30:22 +0300

    com

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -18,7 +18,7 @@
                         <input type="text" name="city" class="form-control" placeholder="{{ __('Şehir veya İlçe') }}" value="{{ request('city') }}">
                     </div>
                     <div class="col-md-2">
-    <button type="submit" class="btn btn-primary search-btn">
+    <button type="submit" class="btn search-btn">
         <i class="fas fa-search"></i> {{ __('İş Bul') }}
     </button>
 </div>
