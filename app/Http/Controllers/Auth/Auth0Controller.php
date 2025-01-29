1a318453af6722e646f168867f75fd6fd257f128
teyfikaktas <aktasteyf@gmail.com>
2025-01-29 23:32:00 +0300

    m

--- app/Http/Controllers/Auth/Auth0Controller.php
+++ app/Http/Controllers/Auth/Auth0Controller.php
@@ -3,56 +3,65 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Auth0\Laravel\Auth0;  // Bu satırı ekleyin
+use Auth0\Laravel\Auth0;
 use Illuminate\Http\Request;
-use Auth;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str; // str_random yerine Str::random kullanacağız 
 use App\Models\User;
 
-class Auth0Controller extends Controller  // Auth0BaseController yerine sadece Controller
+class Auth0Controller extends Controller
 {
-    public function login()
-    {
-        return app('auth0')->login(
-            null,
-            null,
-            ['scope' => 'openid profile email'],
-            'code'
-        );
-    }
+   protected $auth0;
 
-    public function callback()
-    {
-        $auth0User = app('auth0')->getUser();
-    
-        if (!$auth0User) {
-            return redirect()->route('login');
-        }
-    
-        // Kullanıcıyı bul veya oluştur
-        $user = User::updateOrCreate(
-            ['email' => $auth0User['email']],
-            [
-                'name' => $auth0User['name'] ?? null,
-                'auth0_id' => $auth0User['sub'],
-                'password' => Hash::make(str_random(16)),
-                // Google'dan gelen ek bilgiler
-                'photo' => $auth0User['picture'] ?? null, // Google profil fotoğrafı
-                'country' => null, // Google'dan alamıyoruz
-                'city' => null     // Google'dan alamıyoruz
-            ]
-        );
-    
-        Auth::login($user);
-    
-        return redirect()->route('home');
-    }
+   public function __construct(Auth0 $auth0)
+   {
+       $this->auth0 = $auth0;
+   }
 
-    public function logout()
-    {
-        Auth::logout();
-        
-        return app('auth0')->logout(
-            route('login')
-        );
-    }
+   public function login()
+   {
+       return app('auth0')->login(
+           null,
+           null,
+           ['scope' => 'openid profile email'],
+           'code'
+       );
+   }
+
+   public function callback()
+   {
+       $auth0User = app('auth0')->getUser();
+
+       if (!$auth0User) {
+           return redirect()->route('login');
+       }
+
+       // Kullanıcıyı bul veya oluştur
+       $user = User::updateOrCreate(
+           ['email' => $auth0User['email']],
+           [
+               'name' => $auth0User['name'] ?? null,
+               'auth0_id' => $auth0User['sub'],
+               'password' => Hash::make(Str::random(16)),
+               // Google'dan gelen ek bilgiler
+               'photo' => $auth0User['picture'] ?? null, // Google profil fotoğrafı
+               'country' => null, // Google'dan alamıyoruz
+               'city' => null // Google'dan alamıyoruz
+           ]
+       );
+
+       Auth::login($user);
+
+       return redirect()->route('home');
+   }
+
+   public function logout()
+   {
+       Auth::logout();
+       
+       return app('auth0')->logout(
+           route('login')
+       );
+   }
 }
