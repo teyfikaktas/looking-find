2247b40cf8990237522f87b25d90e732ba4a5843
teyfikaktas <aktasteyf@gmail.com>
2025-01-30 19:03:25 +0300

    com

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -40,21 +40,18 @@ class LoginRequest extends FormRequest
     public function authenticate(): void
     {
         $this->ensureIsNotRateLimited();
-    
-        if (! Auth::guard('web')->attempt(
-            $this->only('email', 'password'), 
-            $this->boolean('remember')
-        )) {
+
+        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
-    
+
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
             ]);
         }
-    
+
         RateLimiter::clear($this->throttleKey());
     }
-    
+
     /**
      * Ensure the login request is not rate limited.
      *
